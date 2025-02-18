1601f3735fc5fd4c96ea6fed0827a72fcbb118ba
JubairAnjumRony <rony.ict.iu@gmail.com>
2025-02-18 23:35:37 +0600

    two tone button added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -63,7 +63,7 @@ const Register = () => {
   
       return (
           <div>
-              <div className="hero bg-slate-300 min-h-screen">
+              <div className="hero min-h-screen">
     <div className="hero-content flex-col lg:flex">
     <div className="text-center lg:text-left">
               <h1 className="text-2xl font-bold">Register Now!</h1>
